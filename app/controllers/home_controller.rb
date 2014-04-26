1e7e0373429cc58d242864e7c778d26db6de1ef7
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-04-26 16:13:56 -0400

    Filter event listings by date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,17 @@ class HomeController < ApplicationController
     @time = Time.now
     @search_days = []
     for i in 0...@NUM_DAYS
-      @search_days.append(@time.strftime("%Y-%m-%d"))
+      @search_days.append(@time.strftime("%d+%b+%Y"))
       @time += 86400
     end
 
     # pages directory scrapes from http://www.mysocialist.com/concerts
     doc_string = Nokogiri::HTML(open("pages/mysocialist.txt"))
     @sections = doc_string.css(".listingRow")
-  end
-
-  def get_day
-    @e = params[:date].to_i
-    @e = 0 if @e.nil?
-    @e
+    if params[:date].nil?
+      @date = 0
+    else
+      @date = params[:date].to_i
+    end
   end
 end
